453d4a9a66075130261056864750df8cb176f803
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-12-03 22:15:47 +0200

    nav section now pops in and out

--- app.js
+++ app.js
@@ -10,3 +10,26 @@ const exit = document.querySelector('.fa-times');
 exit.addEventListener('click', function () {
   searchWindow.classList.remove('in');
 });
+
+let results = 2;
+//change the innertext of the search results to reflect number of results
+const resultOutput = document.querySelector('#results-display');
+console.log(resultOutput.textContent);
+function updateResultOutput() {
+  if (results===0) {
+    resultOutput.innerHTML = '<a href="features">Featured</a>'
+  } else {
+    resultOutput.innerHTML = `${results} results returned for that search`;
+  }
+};
+updateResultOutput();
+
+
+//when hamburgur is clicked, add in class to nav menu
+const hamburgur = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('nav');
+const blackHead = document.querySelector('.black-header')
+hamburgur.addEventListener('click', function () {
+  navMenu.classList.toggle('in');
+  blackHead.classList.toggle('out');
+})
