2207ffa62a74676325fa084fc54efb119cd95280
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-12-05 12:03:43 +0200

    main page generate recipe box, pics, name, genre

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-//open the searchbox when you click the magnifying glass
+const hamburgur = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('nav');
+const blackHead = document.querySelector('.black-header');
 const magnifyer = document.querySelector('.header-search-trending');
 const searchWindow = document.querySelector('#search-box');
+const exit = document.querySelector('.fa-times');
+const body = document.querySelector('body');
+
+//open the searchbox when you click the magnifying glass
+
 magnifyer.addEventListener('click', function () {
   searchWindow.classList.add('in');
 });
 
 //close the searchbox when you click the x
-const exit = document.querySelector('.fa-times');
 exit.addEventListener('click', function () {
   searchWindow.classList.remove('in');
 });
@@ -25,9 +31,7 @@ updateResultOutput();
 
 
 //when hamburgur is clicked, add in class to nav menu
-const hamburgur = document.querySelector('.fa-bars');
-const navMenu = document.querySelector('nav');
-const blackHead = document.querySelector('.black-header')
+
 hamburgur.addEventListener('click', function () {
   navMenu.classList.toggle('in');
   blackHead.classList.toggle('out');
@@ -36,10 +40,21 @@ hamburgur.addEventListener('click', function () {
 //parse the json info
 fetch('/data/recipes.json').then(function (response) {
 return response.json();
-}).then(addStuff);
+}).then(fillMainPage);
+
 
 //log out the name of each recipe - let's replace this with
 //all our filling functions.
-function addStuff(stuff) {
-  console.log(stuff.recipes[0].name);
+function fillMainPage(stuff) {
+  const recipeSection = document.createElement('div');
+  recipeSection.classList = 'recipe-section main-page';
+  recipeSection.innerText = "Here's the new recipe section!";
+  body.appendChild(recipeSection);
+  for (recipe of stuff.recipes) {
+    const recipeTile = document.createElement('div');
+    recipeTile.classList = "recipe-tile";
+    recipeTile.innerHTML = '<div class="image-box"><img class="recipe-image" src="img/' + recipe.id + '.jpg" alt="' + recipe.name + '"></div><div class="recipe-genre">'+recipe.genre+'</div><div class="recipe-name">'+recipe.name+'</div>';
+    recipeSection.appendChild(recipeTile);
+    console.log(recipe.name);
+  }
 }
