3554419d718fcabea42e107062323325b3a0de69
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-12-13 20:40:47 +0200

    added recipe page basic outline and tooltips

--- recipe-page.js
+++ recipe-page.js
@@ -1,23 +1,52 @@
+let jsonResponse;
+// Model:
 fetch('/data/recipes.json').then(function (response) {
 return response.json();
-}).then(fillRecipePage);
+}).then(function (jsonFile) {
+  jsonResponse = jsonFile.recipes;
+});
 
-//change page title to first recipe name
-function fillRecipePage(response) {
-  //get recipe id from url
+
+// View:
+
+//Get Tooltips to follow the mouse
+var tooltip = document.querySelectorAll('.tooltip');
+
+document.addEventListener('mousemove', fn, false);
+
+function fn(e) {
+    for (var i=tooltip.length; i--;) {
+        tooltip[i].style.left = (e.pageX-20) + 'px';
+        tooltip[i].style.top = (e.pageY+30) + 'px';
+    }
+}
+
+//Fill large navbar
+const navSection = document.getElementById('')
+
+// Controller:
+class Controller {
+  static getRecipeID() {
     const twoLists = window.location.href.split('html?id=');
     const recipeID = twoLists[1];
     console.log(recipeID);
-//get recipe object using recipe id
-    var recipeObject = (response.recipes.filter(function(e) {
-      return e.id == recipeID;
-    }))[0];
-document.title = recipeObject.name;
-//put in an image of the recipe
-  document.body.innerHTML = '<h1>'+recipeObject.name+'</h1><img src="img/'+recipeObject.id+'.jpg">'
-};
 
-    //if(window.location.href.indexOf("r") > -1) {
-      // alert("your url contains the name franky");
-    //}
-//f});
+    // static getRecipe(e) {
+      // const recipeObject = (response.recipes.filter(function (e) {
+        // return e.id ===
+      // }))
+    // }
+//get recipe object using recipe id
+    // var recipeObject = (response.recipes.filter(function(e) {
+      // return e.id == recipeID;
+    // }))[0];
+  }
+}
+//change page title to first recipe name
+function fillRecipePage(response) {
+  //get recipe id from url
+    Controller.getRecipeID();
+// document.title = recipeObject.name;
+//put in an image of the recipe
+  // document.body.innerHTML = '<h1>'+recipeObject.name+'</h1><img src="img/'+recipeObject.id+'.jpg">'
+};
